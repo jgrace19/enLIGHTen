3f60b60fbdbd3a76a5b0b2a1e9ee0147ecdb80a8
cmguyman11 <cmguyman11@gmail.com>
2018-11-27 12:54:07 -0800

    fixed home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,11 +6,18 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  Button
 } from "react-native";
 import { WebBrowser } from "expo";
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import { MonoText } from "../components/StyledText";
+import LinksScreen from '../screens/LinksScreen';
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -20,22 +27,21 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-        >
 
            <View style={styles.welcomeContainer}>
             <Image
               source={
-                __DEV__
-                  ? require("../assets/images/clouds.png")
-                  : require("../assets/images/clouds.png")
+                require("../assets/images/clouds.png")
               }
             />
-          </View>
 
-        </ScrollView>
+            <Button style={styles.tapIn}   onPress={() => {
+              Debug.log("hello");
+          } } title="Tap In">
+
+          </Button>
+
+          </View>
       </View>
     );
   }
@@ -43,23 +49,12 @@ export default class HomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff"
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
   },
   contentContainer: {
     paddingTop: 30
   },
   welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
   },
   welcomeImage: {
     width: 100,
@@ -68,64 +63,8 @@ const styles = StyleSheet.create({
     marginTop: 3,
     marginLeft: -10
   },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
+  tapIn: {
+    width: 100,
+    height: 80,
   },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
-  }
 });
